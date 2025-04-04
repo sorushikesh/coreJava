ddacf88ef05d89f732a752f03e815ef4b845e002
Rushikesh <sorushikesh07@gmail.com>
2025-04-04 23:36:48 +0530

    DSA: Array : Method to remove duplicates

---
+++ src/main/java/org/array/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package org.array;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that
+     * each unique element appears only once. The relative order of the elements should be kept the same.
+     * Then return the number of unique elements in nums.
+     *
+     * @param nums Integer Array
+     * @return Number of unique elements
+     */
+    public int removeDuplicates(int[] nums) {
+
+    }
+}
