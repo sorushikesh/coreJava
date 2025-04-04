d518b7b25908115f8d2f1f105a1421b5d64e306b
Rushikesh <sorushikesh07@gmail.com>
2025-04-04 23:53:47 +0530

    DSA: Array : Method to remove duplicates

--- src/main/java/org/array/RemoveDuplicates.java
+++ src/main/java/org/array/RemoveDuplicates.java
@@ -1,8 +1,16 @@
 package org.array;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class RemoveDuplicates {
 
+    private static final Logger logger = LogManager.getLogger(RemoveDuplicates.class);
+
     public static void main(String[] args) {
+
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        logger.info("Unique elements from array are {}", removeDuplicates(nums));
 
     }
 
@@ -14,7 +22,18 @@ public class RemoveDuplicates {
      * @param nums Integer Array
      * @return Number of unique elements
      */
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
 
+        if (nums == null || nums.length == 0) return 0;
+        int uniqueIndex = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[uniqueIndex] = nums[i];
+                uniqueIndex++;
+            }
+        }
+
+        return uniqueIndex;
     }
 }
