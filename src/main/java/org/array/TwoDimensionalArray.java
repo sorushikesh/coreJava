fb6c14fb22350dc50c7719bd574b6a664f1bd59e
Rushikesh <sorushikesh07@gmail.com>
2025-04-05 12:16:13 +0530

    DSA: Array : TwoDimensionalArray

--- src/main/java/org/array/TwoDimensionalArray.java
+++ src/main/java/org/array/TwoDimensionalArray.java
@@ -9,25 +9,42 @@ public class TwoDimensionalArray {
 
     public static void main(String[] args) {
 
-//        Declaration of 2D arrays
-//        int[][] numbs = new int[5][5];
-
+        /**
+         * Declaration and initialization of a 2D array.
+         *
+         * A 2D array in Java is an array of arrays. It can be a rectangular array
+         * (all inner arrays have the same length) or a jagged array (each row has a different length).
+         *
+         * Here, we define a jagged array where each row has a different number of elements.
+         */
         int[][] numbs = {
-                {1, 2, 2, 3, 3, 2, 2, 1, 1, 1, 1, 0, 9},
-                {5454, 455, 545454, 455, 5662, 1151, 54545, 89898, 132},
-                {654688, 354168, 31684, 316865, 64681}
+                {1, 2, 2, 3, 3, 2, 2, 1, 1, 1, 1, 0, 9},          // Row 0
+                {5454, 455, 545454, 455, 5662, 1151, 54545, 89898, 132}, // Row 1
+                {654688, 354168, 31684, 316865, 64681}             // Row 2
         };
 
-        // Accessing 2D arrays elements
-        for (int[] num : numbs) {
-            for (int i : num) {
-                logger.info(i);
+        /**
+         * First traversal of the 2D array using enhanced for-loops.
+         * We log each element using Log4j logger.
+         *
+         * Outer loop iterates over each row (int[]),
+         * Inner loop iterates over each element (int) in the current row.
+         */
+        logger.info("Logging each element of the 2D array:");
+        for (int[] row : numbs) {
+            for (int element : row) {
+                logger.info(element);
             }
         }
 
-        for (int[] num : numbs) {
-            for (int i : num) {
-                System.out.print(i + " ");
+        /**
+         * Second traversal of the 2D array to print all elements to console.
+         * Each row is printed in a separate line using System.out.print.
+         */
+        System.out.println("\nPrinting the 2D array to the console:");
+        for (int[] row : numbs) {
+            for (int element : row) {
+                System.out.print(element + " ");
             }
             System.out.println();
         }
