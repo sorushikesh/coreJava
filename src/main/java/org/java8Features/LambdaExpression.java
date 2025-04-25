e045fb8ca384ba0d77713fef1e7e056bf6213e3e
Rushikesh <sorushikesh07@gmail.com>
2025-04-25 18:20:57 +0530

    Code update

---
+++ src/main/java/org/java8Features/LambdaExpression.java
@@ -0,0 +1,9 @@
+package org.java8Features;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LambdaExpression {
+
+  private static final Logger log = LogManager.getLogger(LambdaExpression.class);
+}
